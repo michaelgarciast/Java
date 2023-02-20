b14d36864c99a85fefc0ff904d9faa39008b4f29
michaelgarciast <michaelgarciast>
2023-02-20 18:21:44 -0500

    Taller 01

--- Java/dataStructure/src/PilaStack.java
+++ Java/dataStructure/src/PilaStack.java
@@ -4,9 +4,9 @@
 //     public static void main(String args[]) {
 //         //Last in -> First Out
 //         Stack pila = new Stack<>();
-//         // pila.push("Hola"); //Indice
-//         // pila.push("Hola"); //Indice
-//         // pila.push("Hola mundo"); //Indice
+//         pila.push("Hola"); //Indice
+//         pila.push("Hola"); //Indice
+//         pila.push("Hola mundo"); //Indice
 //         System.out.print("El ultimo elemento de la pila es: " + pila.peek());
 //         while(pila.empty() == false){
 //             System.out.println();

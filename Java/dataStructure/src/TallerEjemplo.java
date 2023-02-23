840370e00ad783984d2c4cf8931d3cc363602cac
michaelgarciast <michaelgarciast>
2023-02-22 22:03:15 -0500

    Commit actual taller ejemplo

--- Java/dataStructure/src/TallerEjemplo.java
+++ Java/dataStructure/src/TallerEjemplo.java
@@ -5,26 +5,27 @@ public class TallerEjemplo {
     private Scanner teclado;
     private float[] altura;
     private float promedio;
+    private int cantEmpleados = 5;
 
     public void dataEmpleados(){
         teclado = new Scanner(System.in);
-        altura = new float[5];
-        for(int i = 0; i < 5; i++){
+        altura = new float[cantEmpleados];
+        for(int i = 0; i < cantEmpleados; i++){
             System.out.println("Ingrese la altura del empleado: " + (i+1) + ": ");
             altura[i] = teclado.nextFloat();
         }
     }
     public void promedio() {
         float suma = 0;
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < cantEmpleados; i++){
             suma = suma + altura[i];
         }
-        promedio = suma / 5;
+        promedio = suma / cantEmpleados;
     }
     public void calcular(){
         int mayores = 0;
         int menores = 0;
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < cantEmpleados; i++){
             if(altura[i] > promedio){
                 mayores ++;
             }else if(altura[i] < promedio){

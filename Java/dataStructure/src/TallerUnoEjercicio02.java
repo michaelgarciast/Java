d634f85234d8f5fbdf5b37b03d3261845d4aeda6
michaelgarciast <michaelgarciast>
2023-02-26 13:43:32 -0500

    new ejercicios

--- Java/dataStructure/src/TallerUnoEjercicio02.java
+++ Java/dataStructure/src/TallerUnoEjercicio02.java
@@ -1,43 +1,45 @@
-//Ejercicio 02
-//La secretaria necesita saber cuántos empleados son más altos que el promedio y cuántas más bajos. 
-//Definir un vector de 20 componentes de tipo float que representen las alturas de los empleados. 
-
 //Librerias
 import java.util.*;
+public class TallerEjemplo {
 
-public class TallerUnoEjercicio02 {
-    public static void main(String args[]) {
-        try (Scanner sc = new Scanner(System.in)) {
-            // Definir el vector de alturas
-            float[] alturas = new float[20];
+    private Scanner teclado;
+    private float[] altura;
+    private float promedio;
+    private int cantEmpleados = 5;
 
-            // Pedir las alturas de los empleados y agregarlas al vector
-            for (int i = 0; i < alturas.length; i++) {
-                System.out.print("Ingrese la altura del empleado " + (i+1) + ": ");
-                alturas[i] = sc.nextFloat();
-            }
-            // Calcular el promedio de alturas
-            float promedio = 0;
-            for (float altura : alturas) {
-                promedio += altura;
-            }
-            promedio /= alturas.length;
-
-            // Contar cuántos empleados son más altos o más bajos que el promedio
-            int mayores = 0;
-            int menores = 0;
-            for (float altura : alturas) {
-                if (altura > promedio) {
-                    mayores++;
-                } else if (altura < promedio) {
-                    menores++;
-                }
-            }
-            // Mostrar los resultados
-            System.out.println("El promedio de alturas es: " + promedio);
-            System.out.println("Hay " + mayores + " empleados más altos que el promedio");
-            System.out.println("Hay " + menores + " empleados más bajos que el promedio");
+    public void dataEmpleados(){
+        teclado = new Scanner(System.in);
+        altura = new float[cantEmpleados];
+        for(int i = 0; i < cantEmpleados; i++){
+            System.out.println("Ingrese la altura del empleado: " + (i+1) + ": ");
+            altura[i] = teclado.nextFloat();
         }
     }
-        
+    public void promedio() {
+        float suma = 0;
+        for(int i = 0; i < cantEmpleados; i++){
+            suma = suma + altura[i];
+        }
+        promedio = suma / cantEmpleados;
+    }
+    public void calcular(){
+        int mayores = 0;
+        int menores = 0;
+        for(int i = 0; i < cantEmpleados; i++){
+            if(altura[i] > promedio){
+                mayores ++;
+            }else if(altura[i] < promedio){
+                menores ++;
+            }
+        }
+        System.out.println("El promedio de alturas es: " + promedio);
+        System.out.println("Hay " + mayores + " empleados más altos que el promedio");
+        System.out.println("Hay " + menores + " empleados más bajos que el promedio");
+    }
+    public static void main(String args[]) {
+        TallerEjemplo vector = new TallerEjemplo();
+        vector.dataEmpleados();
+        vector.promedio();
+        vector.calcular();
+    }
 }

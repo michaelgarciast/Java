2518e5283bd47340e9060778294d766d77b21a32
michaelgarciast <michaelgarciast>
2023-02-27 13:55:27 -0500

    commit tallerunoejercicio1

--- Java/dataStructure/src/TallerUnoEjercicio01.java
+++ Java/dataStructure/src/TallerUnoEjercicio01.java
@@ -5,28 +5,31 @@
 //Librerias
 import java.util.Scanner;
 import java.util.Vector;
-
-
 public class TallerUnoEjercicio01 {
-    public static void main(String args[]) {
-        try (Scanner sc = new Scanner(System.in)) {
+        private Scanner teclado;
+        private int cantOperarios = 5;
+
+        public void sueldosOperarios() {
             // Crear la pila
-            Vector<Float> sueldos = new Vector<Float>(15);
-
-
+            teclado = new Scanner(System.in);
+            Vector<Float> sueldos = new Vector<Float>(cantOperarios);
             // Pedir los sueldos de los operarios y agregarlos a la pila
-            for (int i = 0; i < 15; i++) {
+            for (int i = 0; i < cantOperarios; i++) {
                 System.out.print("Ingrese el sueldo del operario " + (i+1) + ": ");
-                float sueldo = sc.nextFloat();
+                float sueldo = teclado.nextFloat();
                 sueldos.add(sueldo);
             }
-
-             // Acceder a cada elemento de la pila por medio del índice
-             System.out.println("Los sueldos ingresados son:");
-             for (int i = 0; i < sueldos.size(); i++) {
-                 System.out.println("Operario " + (i+1) + ": $" + sueldos.get(i));
-             }
+            // Acceder a cada elemento de la pila por medio del índice
+            System.out.println("Los sueldos ingresados son:");
+            for (int i = 0; i < sueldos.size(); i++) {
+                System.out.println("Operario " + (i+1) + ": $" + sueldos.get(i));
+            }
+            
+        }
+    public static void main(String args[]) {
+        TallerUnoEjercicio01 vector = new TallerUnoEjercicio01();
+        vector.sueldosOperarios();
+        
         }
 
     }
-}
--- Java/dataStructure/src/TallerUnoEjercicio02.java
+++ Java/dataStructure/src/TallerUnoEjercicio02.java
@@ -2,11 +2,13 @@
 import java.util.*;
 public class TallerUnoEjercicio02 {
 
+    //Declaracion de variables
     private Scanner teclado;
     private float[] altura;
     private float promedio;
-    private int cantEmpleados = 5;
+    private int cantEmpleados = 20;
 
+    //Funcion para ingresar los datos de empleados
     public void dataEmpleados(){
         teclado = new Scanner(System.in);
         altura = new float[cantEmpleados];
@@ -15,6 +17,7 @@ public class TallerUnoEjercicio02 {
             altura[i] = teclado.nextFloat();
         }
     }
+    //Funcion para sacar promedio 
     public void promedio() {
         float suma = 0;
         for(int i = 0; i < cantEmpleados; i++){
@@ -22,6 +25,7 @@ public class TallerUnoEjercicio02 {
         }
         promedio = suma / cantEmpleados;
     }
+    //Funcion para calcular la cantidad de mayores y menores
     public void calcular(){
         int mayores = 0;
         int menores = 0;
@@ -32,10 +36,12 @@ public class TallerUnoEjercicio02 {
                 menores ++;
             }
         }
+        //Datos de salida
         System.out.println("El promedio de alturas es: " + promedio);
         System.out.println("Hay " + mayores + " empleados más altos que el promedio");
         System.out.println("Hay " + menores + " empleados más bajos que el promedio");
     }
+    //Inicializacion de funciones
     public static void main(String args[]) {
         TallerUnoEjercicio02 vector = new TallerUnoEjercicio02();
         vector.dataEmpleados();
